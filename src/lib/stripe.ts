9654276633cd5c0327c6ebc801281c06fe735d6b
Vincent Ho <vincenthohoho123@gmail.com>
2025-11-28 21:08:25 -0500

    Update Stripe API version to   2025-10-29.clover

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-04-30.basil',
+  apiVersion: '2025-10-29.clover',
   typescript: true,
 })
